356a830233b615693c03f82b5503e22a1a94d952
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-02-05 20:27:55 +0530

    Create 16_reverseList.c

---
+++ Linked List/16_reverseList.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct linked_list
+{
+	int data; 
+	struct linked_list *next;
+};
+typedef struct linked_list node;
+node *createlist(node *head, int data)
+{
+	node *temp, *start = head;
+	if(head == NULL)
+	{
+		head = (node*)malloc(sizeof(node));
+		head->data = data;
+		head->next = NULL;	
+	}
+	else
+	{
+		while(start->next)
+		{
+			start = start->next;
+		}
+			temp = (node*)malloc(sizeof(node));
+			temp->data = data;
+			temp->next = NULL;
+			start->next = temp;
+	}
+	return head;
+}
+node* reverselist(node *head)
+{
+	node *CURR = head;
+	node *NEXT; 
+	node *PREV = NULL;
+	while(CURR!=NULL)
+	{
+		NEXT = CURR->next;
+		CURR->next = PREV;
+		PREV = CURR;
+		CURR = NEXT;
+	}
+	head = PREV;
+	return head;
+}
+void showlist(node *head)
+{
+	node *temp = head;
+	int x = 0;
+	printf("Here is the list\n");
+	while(temp)
+	{
+		x = temp->data;
+		printf("%d\t",x);
+		temp = temp->next;
+	}
+}
+int main()
+{
+	int i = 0, n = 0;
+	int key;
+	node *head;
+	printf("Enter elements of list\n");
+	for (i = 0;  ; i++)
+	{	
+		scanf("%d",&n);
+		if(n == -9999)
+		{
+			break;
+		}
+		else
+		{
+			head = createlist(head,n);
+		}
+	}
+	head = reverselist(head);
+	showlist(head);
+}
