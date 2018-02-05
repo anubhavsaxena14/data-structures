b70578eb1b8851b08a3cb4b5e88270cc4dd7e898
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-02-05 20:16:28 +0530

    Create length_linked_list.c

---
+++ Linked List/length_linked_list.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct linked_list
+{
+	int data; 
+	struct linked_list *next;
+};
+typedef struct linked_list node;
+node *createlist(node *head, int data)
+{
+	node *temp, *start = head;
+	if(head == NULL)
+	{
+		head = (node*)malloc(sizeof(node));
+		head->data = data;
+		head->next = NULL;	
+	}
+	else
+	{
+		while(start->next)
+		{
+			start = start->next;
+		}
+			temp = (node*)malloc(sizeof(node));
+			temp->data = data;
+			temp->next = NULL;
+			start->next = temp;
+	}
+	return head;
+}
+int listlen(node *head)
+{
+	if(head == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return (1 + listlen(head->next));
+	}
+}
+int main()
+{
+	int i = 0, n = 0, length = 0;
+	node *head;
+	printf("Enter elements of list\n");
+	for (i = 0;  ; i++)
+	{	
+		scanf("%d",&n);
+		if(n == -9999)
+		{
+			break;
+		}
+		else
+		{
+			head = createlist(head,n);
+		}
+	}
+	length = listlen(head);
+	printf("Length of Linked List is %d\n",length);
+}
